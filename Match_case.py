ba391dce5e8caca2086bbd4d87e0657a62884782
Python codes <snehamittal.sm22@gmail.com>
2026-03-31 15:29:58 +0530

    commit

---
+++ Match_case.py
@@ -0,0 +1,11 @@
+light= input("Enter color of traffic light: ")
+
+match light:
+  case "green":
+    print("GO")
+  case "red":
+    print("STOP")
+  case "yellow":
+    print("LOOK")
+  case _:
+    print("Wrong color!")  
