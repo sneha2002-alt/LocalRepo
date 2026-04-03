9cfdbebda47964c43abc3b9e96b619e586009cc2
Python codes <snehamittal.sm22@gmail.com>
2026-04-03 13:30:43 +0530

    commit

---
+++ string_formatting.py
@@ -0,0 +1,14 @@
+a = 5
+b = 10
+sum = a+b
+
+#normal formatting
+print("language is {}".format("python"))
+print("sum of {} & {} is {}".format(a, b, sum))
+
+#index based formatting
+print("sum of {1} & {0} is {2}".format(a, b, sum))
+
+#value based formatting
+print("value of vars {a} & {b}".format(a=10, b=4))
+
