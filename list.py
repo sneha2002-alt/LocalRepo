342b8e78a16df995d97d07e6b46e1f79bf804bd1
Python codes <snehamittal.sm22@gmail.com>
2026-04-04 18:21:08 +0530

    commit

---
+++ list.py
@@ -0,0 +1,31 @@
+# list
+marks = [99, 97, 67, 87, 84, 99]
+print(type(marks))
+print(marks[2])
+marks[2] = 88 # list is mutable
+print(marks[2]) # indexing
+print(marks[:3]) # slicing
+
+# list methods
+nums = [1, 2, 3, 5, 7, 4, 3, 5]
+
+nums.append(10) # add element at the end
+print(nums)
+
+nums.insert(2,9) # insert element at given index
+print(nums)
+
+nums.reverse() #reverses our list
+print(nums)
+
+nums.sort() # sorts in ascending order
+print(nums)
+
+nums.sort(reverse=True) #sorts in descending order
+print(nums)
+
+nums.pop(2)
+print(nums)
+
+nums.remove(3)
+print(nums)
