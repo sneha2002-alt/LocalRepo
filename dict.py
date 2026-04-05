bc28420b72f8d4f78cd1260c2bec387b91f758c2
Python codes <snehamittal.sm22@gmail.com>
2026-04-05 21:29:34 +0530

    commit

---
+++ dict.py
@@ -0,0 +1,14 @@
+# Dictionary
+
+info = {
+  "key" : "value",
+  "name" : "Sneha",
+  "subjects" : ["python", "C", "Java"],
+  "topics" : ("dict", "set"),
+  "age" : "35"
+}
+
+print(info)
+print(type(info))
+
+# key cannot be of datatype list and dictionary
--- tup_methods.py
+++ tup_methods.py
@@ -2,5 +2,5 @@
 
 tup=(6,2,3,4,6,7,8,5)
 
-print(tup.index(2)) # gives index of given element
+print(tup.index(2)) # gives 1st occurrence index of given element
 print(tup.count(6)) # counts no. of occurrences of 6
