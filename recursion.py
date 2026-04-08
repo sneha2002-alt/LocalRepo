10620dcf57f3e0a3315a0cb5858f461b8683a75f
Python codes <snehamittal.sm22@gmail.com>
2026-04-08 09:36:49 +0530

    commit

---
+++ recursion.py
@@ -0,0 +1,9 @@
+# Factorial using Recursion
+def fact(n):
+  if( n == 0 or n == 1):
+    return 1
+  else:
+    return n * fact(n-1)
+  
+num = int(input("Enter number: "))  
+print(f"Factorial of {num}:",fact(num))  
